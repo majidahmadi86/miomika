8468d4120c623b5e876b982b0f06e35e91550d1b
majidahmadi86 <majid.ahmadi86@gmail.com>
2026-05-17 15:49:57 +0700

    fix scroll root level permanent

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -6,13 +6,9 @@ type AppShellProps = {
 
 export function AppShell({ children }: AppShellProps) {
   return (
-    <div className="flex h-svh max-h-svh min-h-0 w-full flex-col overflow-hidden bg-white md:h-full md:max-h-none md:min-h-0">
-      <div className="flex min-h-0 flex-1 flex-col overflow-hidden md:min-h-0">
-        {children}
-      </div>
-      <div className="shrink-0 md:hidden">
-        <BottomNav />
-      </div>
+    <div className="relative flex h-svh max-h-svh flex-col overflow-hidden bg-white md:h-auto md:max-h-none md:overflow-visible">
+      <main className="min-h-0 flex-1 overflow-hidden">{children}</main>
+      <BottomNav />
     </div>
   );
 }
